142c1e11d442e9e56e7595055cbd8535b065caca
Matt Bessey <mbessey@caring.com>
2015-02-07 14:44:05 -0800

    use react-forms instead of rolling my own form shit all over the place

--- app/components/story_editor.jsx
+++ app/components/story_editor.jsx
@@ -5,18 +5,15 @@ var React = require('react'),
   StoryConstants = require('../constants/story_constants'),
   Dispatcher = require('../dispatcher');
 
+var ReactForms = require('react-forms'),
+  Form = ReactForms.Form;
+
 var StoryEditor = React.createClass({
   displayName: 'StoryEditor',
   getInitialState: function () {
     return {
       story: {}
     };
-  },
-  handleChange: function(event) {
-    var keyValue = {story: this.state.story};
-    keyValue.story[event.target.name] = event.target.value;
-
-    this.setState(keyValue);
   },
   componentDidMount: function () {
     var _this = this;
@@ -33,6 +30,7 @@ var StoryEditor = React.createClass({
   },
   render: function () {
     var _this = this, title, submitText;
+    var schema = this._formSchema();
     var commonInputProps = function (name) {
       return {
         name: name,
@@ -57,50 +55,9 @@ var StoryEditor = React.createClass({
             <h2 className="modal-title">{title}</h2>
           </div>
           <form onSubmit={this._save} className="modal-body">
+            <Form key={this.state.id} schema={schema} ref="form" component="div"/>
             <div className="form-group">
-              <label htmlFor="story-story-input">Story Title</label>
-              <input 
-                {...commonInputProps('story')}
-                type="text"
-                required="true"
-                placeholder="As a user, I..." />
-            </div>
-            <div className="form-group">
-              <label htmlFor="story-spec-input">Spec URL</label>
-              <input
-                type="text"
-                {...commonInputProps('spec')} />
-            </div>
-            <div className="form-group">
-              <label htmlFor="story-tech-input">Tech</label>
-              <input 
-                {...commonInputProps('tech')}
-                type="text" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="story-manager-input">Manager</label>
-              <input 
-                {...commonInputProps('manager')}
-                type="text" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="story-epic-input">Epic</label>
-              <input 
-                {...commonInputProps('epic')}
-                type="text" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="story-points-input">Points</label>
-              <input 
-                required="true"
-                {...commonInputProps('points')}
-                type="number" />
-            </div>
-            <div className="form-group">
-              <input  type="submit"
-                      value={submitText}
-                      className="btn btn-primary"
-                      />
+              <button type="submit" className="btn btn-primary">{submitText}</button>
               &nbsp;
               <button className="btn btn-default" data-dismiss="modal">Cancel</button>
             </div>
@@ -109,15 +66,67 @@ var StoryEditor = React.createClass({
       </div>
     )
   },
+  _formSchema: function () {
+    var Scalar = ReactForms.schema.Scalar,
+      Mapping = ReactForms.schema.Mapping,
+      List = ReactForms.schema.List;
+    return Mapping({
+      story:    Scalar({
+        name: 'story',
+        label: 'Story Title',
+        required: true,
+        type: 'string',
+        defaultValue: this.state.story.story
+      }),
+      spec:     Scalar({
+        name: 'spec',
+        label: 'Spec URL',
+        required: true,
+        type: 'string',
+        defaultValue: this.state.story.spec
+      }),
+      tech:     Scalar({
+        name: 'tech',
+        label: 'Tech',
+        required: true,
+        type: 'string',
+        defaultValue: this.state.story.tech
+      }),
+      manager:  Scalar({
+        name: 'manager',
+        label: 'Manager',
+        type: 'string',
+        defaultValue: this.state.story.manager
+      }),
+      epic:     Scalar({
+        name: 'epic',
+        label: 'Epic',
+        type: 'string',
+        defaultValue: this.state.story.epic
+      }),
+      points:   Scalar({
+        name: 'points',
+        label: 'Points',
+        type: 'number',
+        defaultValue: this.state.story.points
+      }),
+    });
+  },
   _save: function (event) {
     event.preventDefault();
-    if(this._editingStory()) {
-      StoryActions.updateStory(this.state.id, this.state.story);
+    var form = this.refs.form;
+    if (form.getValidation().isFailure)  {
+      // force rendering all validation errors
+      form.makeDirty();
     } else {
-      StoryActions.createStory(this.state.story);
+      if(this._editingStory()) {
+        StoryActions.updateStory(this.state.id, this.state.story);
+      } else {
+        StoryActions.createStory(this.state.story);
+      }
+      jQuery('#add-story-dialogue').modal('hide');
+      this.replaceState(this.getInitialState());
     }
-    jQuery('#add-story-dialogue').modal('hide');
-    this.replaceState(this.getInitialState());
   },
   _editingStory: function () {
     return !!this.state.id;
