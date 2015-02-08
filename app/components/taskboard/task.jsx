8a904d5d0054b9637fea85a6817e81329e187233
Matt Bessey <mbessey@caring.com>
2015-02-07 22:11:54 -0800

    move tasks, hide save button

--- app/components/taskboard/task.jsx
+++ app/components/taskboard/task.jsx
@@ -25,7 +25,9 @@ var Task = React.createClass({
     return <div className={`task ${userClass}`}>
       <form onSubmit={this._save}>
         <Form schema={this._schema()} ref="form" component="div"/>
-        <button type="submit">Save</button>
+        <button className="save-button" type="submit">Save</button>
+        <button onClick={this._moveBackward}>«</button>
+        <button onClick={this._moveForward}>»</button>
       </form>
     </div>
   },
@@ -77,6 +79,38 @@ var Task = React.createClass({
   },
   _resetForm() {
     this.refs.form.setValue({});
+  },
+  _moveForward(event) {
+    event.preventDefault();
+    var { taskboardId, storyId, id } = this.props, newState;
+    switch(this.props.task.state) {
+      case 'pending':
+        newState = 'in-progress';
+        break;
+      case 'in-progress':
+        newState = 'complete';
+        break;
+      case 'complete':
+        newState = 'complete';
+        break;
+    }
+    TaskActions.updateTask(taskboardId, storyId, id, {state: newState});
+  },
+  _moveBackward(event) {
+    event.preventDefault();
+    var { taskboardId, storyId, id } = this.props, newState;
+    switch(this.props.task.state) {
+      case 'pending':
+        newState = 'pending';
+        break;
+      case 'in-progress':
+        newState = 'pending';
+        break;
+      case 'complete':
+        newState = 'in-progress';
+        break;
+    }
+    TaskActions.updateTask(taskboardId, storyId, id, {state: newState});
   }
 });
 
